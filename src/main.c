41a5c133341abbf8cacc19d9cb98ec69872e231a
stareyedmoon <stareyedmoon@proton.me>
2025-10-16 11:55:30 +0200

    Remove duplicate tile

--- src/main.c
+++ src/main.c
@@ -13,24 +13,27 @@ void main(void) {
     uint8_t y = 80;
 
     while (1) {
-
         uint8_t inp = joypad();
         if (inp & J_RIGHT) {
             x++;
             set_sprite_tile(0, 2);
+            set_sprite_prop(0, 0);
         }
         if (inp & J_LEFT) {
             x--;
-            set_sprite_tile(0, 4);
+            set_sprite_tile(0, 2);
+            set_sprite_prop(0, S_FLIPX);
         }
         if (inp & J_UP) {
             y--;
-            set_sprite_tile(0, 6);
+            set_sprite_tile(0, 4);
+            set_sprite_prop(0, 0);
         }
         if (inp & J_DOWN) {
             y++;
             set_sprite_tile(0, 0);
-        } 
+            set_sprite_prop(0, 0);
+        }
 
         move_sprite(0, x, y);
 
