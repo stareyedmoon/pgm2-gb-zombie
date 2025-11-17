e2f25de0ddb6c00417e8fa436867c193a392e9d5
stareyedmoon <stareyedmoon@proton.me>
2025-11-17 01:35:12 +0100

    Clean up general.h a little

--- src/include/general.h
+++ src/include/general.h
@@ -9,28 +9,6 @@ uint8_t rand8(void);
 /// @brief Flip all the bits in a byte.
 uint8_t flip_byte(uint8_t value);
 
-
-// 2b tiles. This is the default, and allows for four levels of brighness (or three for sprites).
-#define TILE_FORMAT_2b 0
-// 1b tiles. Takes half as many bits as 2b, but only allows for two levels of brightness.
-#define TILE_FORMAT_1b 1
-// Tiles are compressed with the same format as Pokemon Gen 1 sprites.
-// This saves space, but does come at the cost of longer load times.
-#define TILE_FORMAT_COMPRESSED 2
-
-/// @brief Loads a tile in a format.
-/// @param dest Pointer to the destination.
-/// @param src Pointer to the source.
-/// @param format Format bitmap of the source tile. Use TILE_FORMAT_*
-//void load_tile(uint8_t* dest, uint8_t* src, uint8_t format);
-
-/// @brief Loads some number of tiles in a format.
-/// @param dest Pointer to the destination.
-/// @param src Pointer to the source.
-/// @param format Format bitmap of the source tiles. Use TILE_FORMAT_*
-/// @param count Number of tiles to load.
-//void load_tiles(uint8_t* dest, uint8_t* src, uint8_t format, uint8_t count);
-
 /// @brief Decompresses a sprite to the tile representation.
 /// @param dest Pointer to the buffer to decompress to.
 /// @param src Pointer to the compressed data.
@@ -47,8 +25,9 @@ uint8_t decompress_sprite(uint8_t* dest, uint8_t* src);
 /// @note For signed integers this only works for numbers within INT_MAX of eachother.
 #define DIFF(a, b) ((a) > (b) ? (a) - (b) : (b) - (a))
 
-#define ABS(a) (a < 0 ? -a : a)
+#define ABS(a) ((a) < 0 ? -(a) : (a))
 
-#define SATURATING_SUB(a, b) (b > a ? 0 : a - b)
+/// @brief Computes subtraction with a minimum value of 0.
+#define SATURATING_SUB(a, b) ((b) > (a) ? 0 : (a) - (b))
 
 #endif // _general_h_INCLUDE
